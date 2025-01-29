4028f3d1651af47154fca24044855dca2f6947e5
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-01-29 18:17:39 +0530

    Update NewChat.tsx

--- src/chat/NewChat.tsx
+++ src/chat/NewChat.tsx
@@ -1,14 +1,16 @@
-import NewChatLayout from '@/components/NewChatLayout'
+
 import React from 'react'
 import GenXAIComponent from './GenxAiSection'
 import ChatInput from './ChatInput'
 
 function NewChat() {
   return (
-  <NewChatLayout>
-    <GenXAIComponent/>
-    <ChatInput/>
-  </NewChatLayout>
+
+   <div>
+     <GenXAIComponent/>
+     <ChatInput/>
+   </div>
+
   )
 }
 
