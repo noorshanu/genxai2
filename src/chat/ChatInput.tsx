cafb030e93109f2b151c906ca8a6513c706cd937
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-02-06 17:29:13 +0530

    new

--- src/chat/ChatInput.tsx
+++ src/chat/ChatInput.tsx
@@ -11,16 +11,16 @@ const ChatInput: FC = () => {
         <input
           type="text"
           placeholder="Let the magic begin, Tell GenX your wish!..."
-          className="px-4 py-2 pr-16 bg-transparent border font-sf border-gray-800 rounded-lg text-gray-300 bg-white focus:outline-none w-full "
+          className="px-4 py-2 pr-16 bg-transparent border font-sf border-gray-800  text-gray-300 bg-white rounded-2xl focus:outline-none w-full "
         />
         <button className="absolute right-4 top-1/2 transform -translate-y-1/2">
           <img src="/images/send.png" alt="Send" className="w-6 h-6" />
         </button>
       </div>
    
-      <button className="flex items-center gap-2 py-2 px-4 rounded-lg border border-gray-800 hover:bg-gray-800/50 transition-colors">
+      <button className="flex items-center gap-2 py-2 px-4 bg-black rounded-2xl border border-gray-800 hover:bg-gray-800/50 transition-colors">
         <AiOutlineStop className="text-gray-300" />
-        <span className="text-xs text-gray-300 font-sf">Stop generating</span>
+        <span className="text-sm text-gray-300 font-sf">Stop generating</span>
       </button>
     </div>
   );
--- src/chat/ChatInterface.tsx
+++ src/chat/ChatInterface.tsx
@@ -7,9 +7,14 @@ const ChatInterface: FC = () => {
   return (
     <NewLayout>
       <>
-        <div className="flex flex-col h-[calc(100vh--73px)] sm:h-[calc(100vh-73px)] max-w-4xl mx-auto">
+        <div className="flex flex-col h-[calc(100vh--173px)] sm:h-[calc(100vh-203px)] max-w-4xl mx-auto">
           <MessageList />
-          <ChatInput />
+        
+        </div>
+        <div className=" bg-[#1C1E22] border-t border-[#505B74]">
+        <div className=" max-w-4xl mx-auto pb-[7px] pt-10">
+        <ChatInput />
+        </div>
         </div>
       </>
     </NewLayout>
