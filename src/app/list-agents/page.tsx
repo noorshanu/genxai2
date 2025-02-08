fab8a0b7578061a9511b798829cb5ee3fd111ca4
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-02-08 23:48:55 +0530

    Update page.tsx

--- src/app/list-agents/page.tsx
+++ src/app/list-agents/page.tsx
@@ -4,18 +4,23 @@ import React from 'react'
 
 import Footer from '@/components/Footer'
 import Profilemark from '@/components/Profilemark'
+import AgentDetails from '@/components/AgentDetails'
 
 function page() {
   return (
 <>
 <NewChatLayout>
   
-<div className='flex  flex-col justify-between  h-[100vh] '>
+<div className='flex  flex-col justify-between  ] '>
     <div>
     <h1 className=' pb-2 px-3 text-green-500'>
       Tranding Agents
     </h1>
       <Profilemark/>
+    </div>
+
+    <div>
+      <AgentDetails/>
     </div>
 
 
