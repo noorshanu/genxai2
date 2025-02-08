6bb81ae5470142ba82972b012d9135ece961e8c2
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-02-09 00:52:13 +0530

    c.

--- src/app/list-agents/page.tsx
+++ src/app/list-agents/page.tsx
@@ -11,7 +11,7 @@ function page() {
 <>
 <NewChatLayout>
   
-<div className='flex  flex-col justify-between  ] '>
+<div className='flex  flex-col justify-between   '>
     <div>
     <h1 className=' pb-2 px-3 text-green-500'>
       Tranding Agents
@@ -19,8 +19,17 @@ function page() {
       <Profilemark/>
     </div>
 
-    <div>
+    <div className=' flex items-center'>
       <AgentDetails/>
+
+      <div className="flex gap-2 p-4 justify-center">
+      <input
+        type="text"
+        placeholder="Search Agent"
+        className="px-4 py-2 rounded-lg bg-gray-700 text-white w-64 sm:w-80 focus:outline-none"
+      />
+      <button className="px-4 py-2 bg-indigo-500 text-white rounded-lg">Enter</button>
+    </div>
     </div>
 
 
--- src/components/AgentDetails.tsx
+++ src/components/AgentDetails.tsx
@@ -1,93 +1,75 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 
-
-
 function AgentDetails() {
   return (
     <div className="bg-black  text-white  overflow-hidden">
-
       <div className=" flex justify-between px-4 items-center">
         <div className="flex gap-4 items-center">
-        <div className='bg-[#35425A] rounded-lg p-2 flex mx-2  w-full gap-4 relative'>
-        <div className=' flex gap-2 w-full'>
-          <img src="images/nft3.png" alt="" className='h-14 w-14' />
-          <div>
-            <p className=' font-bold text-[10px] '>
-              Agenx ($agenx)
+          <div className="bg-[#35425A] rounded-lg p-2 flex mx-2  w-full gap-4 relative">
+            <div className=" flex gap-2 w-full">
+              <img src="images/nft3.png" alt="" className="h-14 w-14" />
+              <div>
+                <p className=" font-bold text-[10px] ">Agenx ($agenx)</p>
+                <p className=" text-[10px] text-[#31FF5E] font-bold">
+                  {" "}
+                  Market Cap : $400.6k
+                </p>
+                <p className="text-[8px] font-semibold ">Replies : 700+</p>
+                <p className="text-[8px] text-gray-300">Creator:XYZ</p>
+              </div>
+            </div>
+            <div>
+              <img src="images/arrow.png" alt="" />
+            </div>
+            <p className=" text-gray-300 text-[8px] absolute bottom-2 right-4">
+              5min ago
             </p>
-            <p className=' text-[10px] text-[#31FF5E] font-bold' > Market Cap : $400.6k</p>
-            <p className='text-[8px] font-semibold '>
-              Replies : 700+
+          </div>
+
+          <div className="bg-[#35425A] rounded-lg p-2 flex mx-2  w-full gap-4 relative">
+            <div className=" flex gap-2 w-full">
+              <img src="images/nft3.png" alt="" className="h-14 w-14" />
+              <div>
+                <p className=" font-bold text-[10px] ">Agenx ($agenx)</p>
+                <p className=" text-[10px] text-[#31FF5E] font-bold">
+                  {" "}
+                  Market Cap : $400.6k
+                </p>
+                <p className="text-[8px] font-semibold ">Replies : 700+</p>
+                <p className="text-[8px] text-gray-300">Creator:XYZ</p>
+              </div>
+            </div>
+            <div>
+              <img src="images/arrow.png" alt="" />
+            </div>
+            <p className=" text-gray-300 text-[8px] absolute bottom-2 right-4">
+              5min ago
             </p>
-            <p className='text-[8px] text-gray-300'>Creator:XYZ</p>
+          </div>
+
+          <div className="bg-[#35425A] rounded-lg p-2 flex mx-2  w-full gap-4 relative">
+            <div className=" flex gap-2 w-full">
+              <img src="images/nft3.png" alt="" className="h-14 w-14" />
+              <div>
+                <p className=" font-bold text-[10px] ">Agenx ($agenx)</p>
+                <p className=" text-[10px] text-[#31FF5E] font-bold">
+                  {" "}
+                  Market Cap : $400.6k
+                </p>
+                <p className="text-[8px] font-semibold ">Replies : 700+</p>
+                <p className="text-[8px] text-gray-300">Creator:XYZ</p>
+              </div>
+            </div>
+            <div>
+              <img src="images/arrow.png" alt="" />
+            </div>
+            <p className=" text-gray-300 text-[8px] absolute bottom-2 right-4">
+              5min ago
+            </p>
           </div>
         </div>
-        <div>
-          <img src="images/arrow.png" alt="" />
-
-        </div>
-        <p className=' text-gray-300 text-[8px] absolute bottom-2 right-4'>
-          5min ago
-        </p>
       </div>
-
-
-
-
-      <div className='bg-[#35425A] rounded-lg p-2 flex mx-2  w-full gap-4 relative'>
-        <div className=' flex gap-2 w-full'>
-          <img src="images/nft3.png" alt="" className='h-14 w-14' />
-          <div>
-            <p className=' font-bold text-[10px] '>
-              Agenx ($agenx)
-            </p>
-            <p className=' text-[10px] text-[#31FF5E] font-bold' > Market Cap : $400.6k</p>
-            <p className='text-[8px] font-semibold '>
-              Replies : 700+
-            </p>
-            <p className='text-[8px] text-gray-300'>Creator:XYZ</p>
-          </div>
-        </div>
-        <div>
-          <img src="images/arrow.png" alt="" />
-
-        </div>
-        <p className=' text-gray-300 text-[8px] absolute bottom-2 right-4'>
-          5min ago
-        </p>
-      </div>
-
-
-
-
-      <div className='bg-[#35425A] rounded-lg p-2 flex mx-2  w-full gap-4 relative'>
-        <div className=' flex gap-2 w-full'>
-          <img src="images/nft3.png" alt="" className='h-14 w-14' />
-          <div>
-            <p className=' font-bold text-[10px] '>
-              Agenx ($agenx)
-            </p>
-            <p className=' text-[10px] text-[#31FF5E] font-bold' > Market Cap : $400.6k</p>
-            <p className='text-[8px] font-semibold '>
-              Replies : 700+
-            </p>
-            <p className='text-[8px] text-gray-300'>Creator:XYZ</p>
-          </div>
-        </div>
-        <div>
-          <img src="images/arrow.png" alt="" />
-
-        </div>
-        <p className=' text-gray-300 text-[8px] absolute bottom-2 right-4'>
-          5min ago
-        </p>
-      </div>
-        </div>
-
-      </div>
- 
-     
     </div>
   );
 }
